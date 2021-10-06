d7d68a6300f0348589086fd379dc8d76f4f7005e
Daria Liferova <liferova.daria@gmail.com>
2021-10-06 14:12:37 +0200

    Создала компонент для приватных маршрутов. Добавила логику приватности  в соответсвтии с ТЗ.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import {AppProps} from './app-types';
-import {AppRoute} from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import FavoritesScreen from '../favorites-screen/favorites-screen';
 import LoginScreen from '../login-screen/login-screen';
 import MainScreen from '../main-screen/main-screen';
 import NotFoundScreen from '../not-fount-screen/not-found-screen';
 import PropertyScreen from '../property-screen/property-screen';
+import PrivateRoute from '../private-route/private-route';
 
 function App({placesCount}: AppProps): JSX.Element {
   return (
@@ -19,9 +20,13 @@ function App({placesCount}: AppProps): JSX.Element {
         <Route exact path={AppRoute.SignIn}>
           <LoginScreen />
         </Route>
-        <Route exact path={AppRoute.Favorites}>
-          <FavoritesScreen />
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <FavoritesScreen />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
         <Route exact path={AppRoute.Room}>
           <PropertyScreen />
         </Route>
