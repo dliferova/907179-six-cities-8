a486f5ca166208fdb76a3f524127eca82684722d
Daria Liferova <liferova.daria@gmail.com>
2021-10-24 19:02:16 +0200

    Подключила redux к проекту. Добавила поддержку инструмента Redux DevTools.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {reducer} from './store/reducer';
+
 import {offersMock} from './mocks/offers-mock';
 import {reviewsMock} from './mocks/reviews-mock';
 
 const PLACES_COUNT = 458;
 
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      placesCount = {PLACES_COUNT}
-      offers={offersMock}
-      reviews={reviewsMock}
-    />
+    <Provider store={store}>
+      <App
+        placesCount = {PLACES_COUNT}
+        offers={offersMock}
+        reviews={reviewsMock}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
