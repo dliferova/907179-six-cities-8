ed04cad7bac19c25bfc9aebef05e4b1e9c790a83
Daria Liferova <liferova.daria@gmail.com>
2021-11-01 22:25:08 +0100

    Добавила логику отображения header в зависимости от статуса пользователя.

---
+++ project/src/components/header/user-logged-view.tsx
@@ -0,0 +1,46 @@
+import {Link} from 'react-router-dom';
+import {connect, ConnectedProps} from 'react-redux';
+import {ThunkAppDispatch} from '../../types/action';
+import {logoutRequired} from '../../store/actions';
+import {AppRoute} from '../../const';
+
+const mapDispatchToProps = (dispatch: ThunkAppDispatch) => ({
+  onLogoutClicked() {
+    dispatch(logoutRequired());
+  },
+});
+
+const connector = connect(null, mapDispatchToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+//TODO заменить на email 23
+
+function UserLoggedViewBar({onLogoutClicked}: PropsFromRedux): JSX.Element {
+  return (
+    <>
+      <li className="header__nav-item user">
+        <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+          <div className="header__avatar-wrapper user__avatar-wrapper">
+          </div>
+          <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+        </Link>
+      </li>
+      <li className="header__nav-item">
+
+        <a className="header__nav-link"
+          href="#"
+          onClick={(evt) => {
+            evt.preventDefault();
+            onLogoutClicked();
+          }}
+        >
+          <span className="header__signout">Sign out</span>
+        </a>
+
+      </li>
+    </>
+  );
+}
+
+export {UserLoggedViewBar};
+export default connector(UserLoggedViewBar);
