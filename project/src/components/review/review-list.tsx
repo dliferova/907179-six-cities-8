eab1ba60478511fb6a94392af434d8064677f8cf
Daria Liferova <liferova.daria@gmail.com>
2021-11-17 17:53:40 +0100

    Доработала секцию отзывов в соответствии с ТЗ.

--- project/src/components/review/review-list.tsx
+++ project/src/components/review/review-list.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import {Reviews} from '../../types/reviews';
+import {Review, Reviews} from '../../types/reviews';
 import ReviewItem from './review-list-item';
+import dayjs from 'dayjs';
 
 type ReviewListProps = {
   reviews: Reviews;
 }
 
+const MAX_REVIEW_COUNT = 10;
+
 function ReviewList({reviews}: ReviewListProps): JSX.Element {
+  const userReviews = [...reviews]
+    .sort((prev: Review, next: Review) => dayjs(next.date).diff(dayjs(prev.date)))
+    .slice(0, MAX_REVIEW_COUNT);
+
   return (
     <>
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{userReviews.length}</span></h2>
       <ul className="reviews__list">
         {
-          reviews.map((review) => (
-            <ReviewItem
-              key={review.id}
-              review={review}
-            />
-          ))
+          userReviews
+            .map((review) => (
+              <ReviewItem
+                key={review.id}
+                review={review}
+              />
+            ))
         }
       </ul>
     </>
